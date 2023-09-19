9a5e2cdde5b4c695c9845a8a739e912b91fdb5ec
blaharos <blaharos@fit.cvut.cz>
2023-09-19 11:50:40 +0200

    Filters done.

--- my-favorite-cinnabons/src/App.tsx
+++ my-favorite-cinnabons/src/App.tsx
@@ -3,14 +3,15 @@ import './App.css';
 import CinnabonList from './CinnabonList';
 
 const cinnabons = [
-	{ name: 'Brauð & co.', location: '16 Frakkastígur, 101 Reykjavík, Iceland', rating: 10 },
-	{ name: 'Café Pilot', location: 'Dačického 1225/8, 140 00 Praha 4-Nusle, Czechia', rating: 8 },
-	{ name: 'Eaternia', location: 'Nádražní 349/3, 150 00 Praha 5-Smíchov, Czechia', rating: 8 },
-	{ name: 'U Kalendů (Babka)', location: 'Rašínovo nábř. 383/58, 128 00 Nové Město, Czechia', rating: 7 },
-	{ name: 'Tady a teď', location: 'Na Bělidle 31, 150 00 Praha 5-Smíchov, Czechia', rating: 6 },
-	{ name: 'Artic Bakehouse', location: 'Štefánikova 31, 150 00 Praha 5-Smíchov, Czechia', rating: 4.5 },
-	{ name: 'Delmart', location: 'Nádražní 344, 150 00 Praha 5-Anděl, Czechia', rating: 3 },
+	{ name: 'Brauð & co.', location: '16 Frakkastígur, 101 Reykjavík', country: 'Iceland', rating: 10 },
+	{ name: 'Café Pilot', location: 'Dačického 1225/8, 140 00 Praha 4-Nusle', country: 'Czechia', rating: 8 },
+	{ name: 'Eaternia', location: 'Nádražní 349/3, 150 00 Praha 5-Smíchov', country: 'Czechia', rating: 8 },
+	{ name: 'U Kalendů (Babka)', location: 'Rašínovo nábř. 383/58, 128 00 Nové Město', country: 'Czechia', rating: 7 },
+	{ name: 'Tady a teď', location: 'Na Bělidle 31, 150 00 Praha 5-Smíchov', country: 'Czechia', rating: 6 },
+	{ name: 'Artic Bakehouse', location: 'Štefánikova 31, 150 00 Praha 5-Smíchov', country: 'Czechia', rating: 4.5 },
+	{ name: 'Delmart', location: 'Nádražní 344, 150 00 Praha 5-Anděl', country: 'Czechia', rating: 3 }
 ];
+
 
 function App() {
 	return (
---
+++ my-favorite-cinnabons/src/FilterComponent.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+type FilterProps = {
+	countries: string[];
+	selectedCountries: string[];
+	order: 'most' | 'least';
+	onSelectCountry: (country: string) => void;
+	toggleOrder: () => void;
+};
+
+const FilterComponent: React.FC<FilterProps> = ({ countries, selectedCountries, order, onSelectCountry, toggleOrder }) => {
+	const [displayCountries, setDisplayCountries] = useState<string[]>([]);
+	const [isLeastToMost, setIsLeastToMost] = useState(false);
+	const [playAnimation, setPlayAnimation] = useState(false);
+
+	useEffect(() => {
+		setDisplayCountries(countries);
+	}, [countries]);
+
+	return (
+		<div style={{
+			fontFamily: 'Fira Sans, sans-serif',
+			fontSize: '1.5em',
+			color: 'white'
+		}}>
+			<span>Cinnabons in </span>
+			{displayCountries.map((country, index) => (
+				<span key={index}>
+					<span
+						className={selectedCountries.includes(country) ? 'cross-out' : 'selected cross-out'}
+						onClick={() => onSelectCountry(country)}
+					>
+						<strong>{country}</strong>
+					</span>
+					{index !== displayCountries.length - 1 && ' and '}
+				</span>
+			))}
+			<span> ordered from my </span>
+			<span
+				className={`switch-order ${playAnimation? 'switched' : ''}`}
+				onClick={() => {
+					setIsLeastToMost(!isLeastToMost);
+					setPlayAnimation(false);
+					toggleOrder();
+				}}
+				onMouseEnter={() => setPlayAnimation(true)}
+				onMouseLeave={() => setPlayAnimation(false)}
+			>
+				<strong className={playAnimation ? 'order-word' : ''} id="most">{isLeastToMost ? 'least' : 'most'}</strong>
+				<span>{' to '}</span>
+				<strong className={playAnimation ? 'order-word' : ''} id="least">{isLeastToMost ? 'most' : 'least'}</strong>
+				<span>{' favorite.'}</span>
+			</span>
+		</div>
+	);
+};
+
+export default FilterComponent;
